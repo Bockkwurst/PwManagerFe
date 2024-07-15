fb715b96ca0471243548e4cc2fffa1f036a7b97a
Bockkwurst <147827734+Bockkwurst@users.noreply.github.com>
2024-07-15 14:42:43 +0200

    navbar ready

--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterOutlet} from "@angular/router";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterOutlet,
+    NgClass
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
